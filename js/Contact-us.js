b8e3ebf76968a66ad335d76e54f8cdab479f6b4c
RinesaB <74425880+RinesaB@users.noreply.github.com>
2021-01-13 00:06:10 +0100

    Add files via upload

---
+++ js/Contact-us.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.btn-down').click(function(){
+            $('html,body').animate({scrollTop: $('.section-forum').offset().top}, 10); 
+        });
+    $('section-forum').waypoint(function(){
+        
+    });
+
+});
