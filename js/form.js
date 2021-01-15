5bdb503ab154f7a427e15cbddf8b306f558ef418
Qendrim1 <74424873+Qendrim1@users.noreply.github.com>
2021-01-15 23:32:49 +0100

    Update form.js

--- js/form.js
+++ js/form.js
@@ -2,146 +2,141 @@ function Validimi() {
   var Mesazhi, x;
   Mesazhi = document.getElementById("id1");
   x = document.getElementById("fname").value;
-  try {
-    if(x == "") throw "This feils should not be empty";
-    if(x.match(/[0-9]/)) throw "Name only contains characters";
-    if(x.match(/[A-Za-z]+[0-9]+[!@#$%^&*()_+":><?]/)) throw "Name only contains characters";
-    if(x.match(/[A-Za-z]/)) throw null;
+
+  if(x == ""|| x.match(/[0-9]/)|| x.match(/[A-Za-z]+[0-9]+[!@#$%^&*()_+":><?]/)){
+    Mesazhi.innerHTML = "keni shneuar merin gabim";
   }
-  catch(err) {
-      Mesazhi.innerHTML = err;
+  else{
+    Mesazhi.innerHTML = "";
   }
- 
   //==============================================================================================
   //==============================================================================================
   var Mesazhi1 , y;
   Mesazhi1=document.getElementById("id2");
   y = document.getElementById("fsurname").value;
-   try {
-    if(y == "") throw "This feils should not be empty";
-    if(y.match(/[0-9]/)) throw "Name only contains characters";
-    if(y.match(/[A-Za-z]+[0-9]/)) throw "Name only contains characters";
-    if(x.match(/[A-Za-z]/)) throw null;
-    }
-    catch(err) {
-        Mesazhi1.innerHTML = err;
-    }
+  if(y == ""|| y.match(/[0-9]/)|| y.match(/[A-Za-z]+[0-9]/)){
+    Mesazhi1.innerHTML = "keni shneuar merin gabim";
+  }
+  else{
+    Mesazhi1.innerHTML = "";
+  }
+
   //==============================================================================================
   //==============================================================================================
   var Mesazhi2 , a;
   Mesazhi2=document.getElementById("id3");
   a = document.getElementById("ftel").value;
-    try {
-      if(a == "") throw "This feils should not be empty";
-      if(a.match(/[A-Za-z]/)) throw "Telephone only contains numbers";
-      if(a.match(/[A-Za-z]+[0-9]/)) throw "Telephone only contains numbers";
-      if(a.length>12 || a.length<12) throw "Telephone length must be 10 digit";
-      if(a.length == 12) throw null;
-      }
-      catch(err) {
-          Mesazhi2.innerHTML = err;
-      }
-  //==============================================================================================
+  if(a == ""|| a.match(/[A-Za-z]/)|| a.match(/[A-Za-z]+[0-9]/)){
+    Mesazhi2.innerHTML = "keni shneuar merin gabim";
+  }
+  else if(a.length>12 || a.length<12){
+    Mesazhi2.innerHTML = "Telephone length must be 12 digit";
+  }
+  else{
+    Mesazhi2.innerHTML = "";
+  }
+//==========================================================================================
   //==============================================================================================
   var Mesazhi3 , b;
   Mesazhi3=document.getElementById("id4");
   b = document.getElementById("fpersonal").value;
-    try {
-      if(b == "") throw "This feils should not be empty";
-      if(b.match(/[A-Za-z]/)) throw "Personal number only contains numbers";
-      if(b.match(/[A-Za-z]+[0-9]/)) throw "Personal number only contains numbers";
-      if(b.length>10 || b.length<10) throw "Personal number length must be 10 digit";
-      if(b.length==10) throw null;
-      }
-      catch(err) {
-          Mesazhi3.innerHTML = err;
-      }
+  if(b == ""|| b.match(/[A-Za-z]/)|| b.match(/[A-Za-z]+[0-9]/)){
+    Mesazhi3.innerHTML = "keni shneuar merin gabim";
+  }
+  else if(b.length>10 || b.length<10){
+    Mesazhi3.innerHTML = "Personal number length must be 10 digit";
+  }
+  else{
+    Mesazhi3.innerHTML = "";
+  }
   //==============================================================================================
   //==============================================================================================
   var Mesazhi4 , c;
   Mesazhi4=document.getElementById("id5");
   c = document.getElementById("femail").value;
   var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-  try{
-  if(c.match(mailformat)) throw null;
-  else throw "Invalid format of email, email must be like: someone@domain.com"
+  if(c.match(mailformat)){
+    Mesazhi4.innerHTML = "";
   }
-  catch(err){
-      Mesazhi4.innerHTML = err;
+  else{
+    Mesazhi4.innerHTML ="Invalid format of email, email must be like: someone@domain.com"
   }
+
   //==============================================================================================
   //==============================================================================================
   var Mesazhi5, d;
   Mesazhi5 = document.getElementById("id6");
   d = document.getElementById("fresidenca").value;
-  try {
-    if(d == "") throw "This feils should not be empty";
-    if(d.match(/[0-9]/)) throw "Name only contains letters";
-    if(d.match(/[A-Za-z]+[0-9]+[!@#$%^&*()_+":><?]/)) throw "Name only contains letters";
-    if(d.match(/[A-Za-z]/)) throw null;
+  if(d == ""|| d.match(/[0-9]/)|| d.match(/[A-Za-z]+[0-9]+[!@#$%^&*()_+":><?]/)){
+    Mesazhi5.innerHTML = "keni shneuar merin gabim";
   }
-  catch(err) {
-      Mesazhi5.innerHTML = err;
+  else{
+    Mesazhi5.innerHTML = "";
   }
+
   //==============================================================================================
   //==============================================================================================
   var Mesazhi6, e;
   Mesazhi6 = document.getElementById("id7");
   e = document.getElementById("fdestination").value;
-  try {
-    if(e == "") throw "This feils should not be empty";
-    if(e.match(/[0-9]/)) throw "Name only contains letters";
-    if(e.match(/[A-Za-z]+[0-9]+[!@#$%^&*()_+":><?]/)) throw "Name only contains letters";
-    if(e.match(/[A-Za-z]/)) throw null;
+  if(e == ""|| e.match(/[0-9]/)|| e.match(/[A-Za-z]+[0-9]+[!@#$%^&*()_+":><?]/)){
+    Mesazhi6.innerHTML = "keni shneuar merin gabim";
   }
-  catch(err) {
-      Mesazhi6.innerHTML = err;
+  else{
+    Mesazhi6.innerHTML = "";
   }
   //==============================================================================================
   //==============================================================================================
   var Mesazhi7, f;
   Mesazhi7 = document.getElementById("id8");
   f = document.getElementById("fticket").value;
-  try {
-    if(f == "") throw "This feils should not be empty";
-    if(f.match(/[A-Za-z]/)) throw "Personal number only contains numbers";
-    if(f.match(/[A-Za-z]+[0-9]/)) throw "Personal number only contains numbers";
-    if(f.length>100) throw "ticket number must be with 2 digit";
-    if(f.length<10) throw null;
+  if(f == ""|| f.match(/[A-Za-z]/)|| f.match(/[A-Za-z]+[0-9]/)){
+    Mesazhi7.innerHTML = "keni shneuar merin gabim";
   }
-  catch(err) {
-      Mesazhi7.innerHTML = err;
+  else if(f.length>100){
+    Mesazhi7.innerHTML = "ticket numer is lower than 100";
   }
+  else{
+    Mesazhi7.innerHTML = "";
+  }
+  
   //==============================================================================================
   //==============================================================================================
   var Mesazhi8, g;
   Mesazhi8 = document.getElementById("id9");
   g = document.getElementById("fprice").value;
-  try {
-    if(g == "") throw "This field should not be empty";
-    if(g.match(/[A-Za-z]/)) throw "price only contains numbers";
-    if(g.match(/[A-Za-z]+[0-9]/)) throw "price only contains numbers"
-    if(g.length>15) throw "price cant be larger than 15 digit";
-    if(g.length<10) throw null;
+  if(g == ""|| g.match(/[A-Za-z]/)|| g.match(/[A-Za-z]+[0-9]/)){
+    Mesazhi8.innerHTML = "keni shneuar merin gabim";
   }
-  catch(err) {
-      Mesazhi8.innerHTML = err;
+  else if(b.length>15){
+    Mesazhi8.innerHTML = "price is lower than 15 digit";
   }
+  else{
+    Mesazhi8.innerHTML = "";
+  }
+
   //==============================================================================================
   //==============================================================================================
   var Mesazhi9, h;
   Mesazhi9 = document.getElementById("id10");
   h = document.getElementById("fbank").value;
-  try {
-    if(h == "") throw "This field should not be empty";
-    if(h.match(/[A-Za-z]/)) throw "bank account only contains numbers";
-    if(h.match(/[A-Za-z]+[0-9]/)) throw "bank account only contains numbers"
-    if(h.length>16 || h.length<16 ) throw "bank account cant be larger than 16 digit";
-    if(h.length==16) throw null;
+  if(h == ""|| h.match(/[A-Za-z]/)|| h.match(/[A-Za-z]+[0-9]/)){
+    Mesazhi9.innerHTML = "keni shneuar merin gabim";
   }
-  catch(err) {
-      Mesazhi9.innerHTML = err;
+  else if(h.length>16 || h.length<16 ){
+    Mesazhi9.innerHTML = "bank accoun number must be 16 digit";
   }
- 
- 
+  else{
+    Mesazhi9.innerHTML = "";
+  }
+  //==============================================================================================
+  //==============================================================================================
+  if(x.match(/[A-Za-z]/) && y.match(/[A-Za-z]/) && a.length == 12 && b.length==10
+   && c.match(mailformat) && d.match(/[A-Za-z]/) && e.match(/[A-Za-z]/)
+     && f.length<10 && g.length<10 && h.length==16){
+    alert("te dhenat u derguan");
+    location.reload();
+  }
 }
+
+  
